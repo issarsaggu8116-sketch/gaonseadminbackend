c7bee0d367137c1c012425a373c97ddad4ec195a
issarsaggu8116-sketch <issarsaggu8116@gmail.com>
2026-05-16 18:50:42 +0530

    Update inventoryRoutes.js

--- routes/inventoryRoutes.js
+++ routes/inventoryRoutes.js
@@ -8,10 +8,10 @@ import {
 
 const router = express.Router();
 
-/* 📦 FETCH ALL INVENTORY */
+/* FETCH ALL INVENTORY */
 router.get("/", getInventory);
 
-/* 🔄 RESET INVENTORY */
+/*  RESET INVENTORY */
 router.put("/reset", resetInventory);
 
 export default router;
