8c215ee944f07dd83327728ea525a26d28de8a17
issarsaggu8116-sketch <issarsaggu8116@gmail.com>
2026-05-23 23:56:46 +0530

    Update adminDashboardRoutes.js

--- routes/adminDashboardRoutes.js
+++ routes/adminDashboardRoutes.js
@@ -3,13 +3,14 @@ import {
   getDashboard,
   resetProductStock,
 } from "../controllers/adminDashboardController.js";
+import { isAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
 
 // 📊 GET ALL PRODUCTS STOCK
-router.get("/dashboard", getDashboard);
+router.get("/dashboard", isAuthenticated, getDashboard);
 
 // 🔄 RESET PARTICULAR PRODUCT STOCK
-router.put("/reset-stock/:productId", resetProductStock);
+router.put("/reset-stock/:productId", isAuthenticated, resetProductStock);
 
 export default router;
